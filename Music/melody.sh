86328d42f7eca1659baaf8d77ff4f360cc17b16c
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-10-03 17:36:03 -0500

    melody updated

--- Music/melody.sh
+++ Music/melody.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-python music.py
-mv out.wav out1.wav
-python music.py
-mv out.wav out2.wav
-sox out1.wav out2.wav out1.wav out1.wav out2.wav out2.wav out1.wav out1.wav out2.wav out2.wav out1.wav out1.wav out.wav fade t 4 0 4
+python real_music.py
+
 sox bass.wav newbass.wav gain +8
-sox -m newbass.wav out.wav final.wav
+sox -m newbass.wav guitar.wav final.wav fade 4 0 4
