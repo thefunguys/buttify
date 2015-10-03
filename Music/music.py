76ee5e82d41c41a521d31295ed92c21caeb8fee4
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-10-03 05:19:34 -0500

    we musical

---
+++ Music/music.py
@@ -0,0 +1,94 @@
+import numpy as np
+import random
+
+root = 0
+good = 1
+ok = 2
+bad = 3
+size_rhythm = 4
+
+
+
+def getNote(array):
+    x = random.random()
+    for i in range(len(array)):
+
+        if x < array[i]:
+            break
+    return i+1
+
+def makeArray(Notes):
+    pgood = 0.75
+    pok = 0.2
+    pbad = 0.05
+    ngood = 0.
+    nok = 0.
+    nbad = 0.
+    for i in Notes:
+        if(i == root):
+            ngood = ngood + 2.
+        elif(i == good):
+            ngood = ngood + 1.
+        elif(i == ok):
+            nok = nok + 1.
+        else:
+            nbad = nbad + 1.
+
+    prob = 0.0
+    for i in range(len(Notes)):
+        if(Notes[i] == root):
+            Notes[i] = 2*pgood/ngood + prob
+        elif(Notes[i] == good):
+            Notes[i] = pgood/ngood + prob
+        elif(Notes[i] == ok):
+            Notes[i] = pok/nok + prob
+        else:
+            Notes[i] = pbad/nbad + prob
+
+        prob = Notes[i]
+
+    return Notes
+
+def pickBassBeat(Array):
+    picks = [0] * size_rhythm
+    for i in range(size_rhythm):
+        x = random.randint(0,len(Array))
+        picks[i]= x
+
+    return picks
+
+
+M7 = [0,3,2,3,1,2,3,1,3,2,3,1,1]
+m7 = [0,3,2,1,3,2,3,1,2,3,1,3,1]
+Mpent = [0,3,2,3,1,1,3,1,3,2,3,2,1]
+mpent = [0,3,2,1,3,1,3,1,2,3,2,3,1]
+m7 = makeArray(m7)
+M7 = makeArray(M7)
+Mpent = makeArray(Mpent)
+mpent = makeArray(mpent)
+
+
+rhyArr = []
+rhyArr.append([2,2,2,2])
+rhyArr.append([4,4])
+rhyArr.append([2,2,4])
+rhyArr.append([2,4,2])
+rhyArr.append([0,0,4,0,0])
+
+pickBassBeat(rhyArr)
+num_notes=0
+music = []
+for i in range(size_rhythm):
+    for j in rhyArr[i]:
+        note = 0
+        if j != 0:
+            note = getNote(m7)
+        music.append([note,j])
+
+
+print music
+
+
+
+
+
