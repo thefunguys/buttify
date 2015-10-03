954a8bbb4647bfc59fa4f0f93ce739ad6e16c0ec
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-10-03 16:09:25 -0500

    more prot

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import subprocess
 from flask import Flask
 
 app = Flask(__name__)
-app.config.SERVER_NAME = 'localhost:8080'
+app.config['SERVER_NAME'] = 'localhost:8080'
 
 @app.route('/')
 def get_song():
