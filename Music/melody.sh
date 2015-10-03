d9df5ef9738155701c0ddc74de88a4081ab901b9
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-10-03 15:54:35 -0500

    Bass in song

--- Music/melody.sh
+++ Music/melody.sh
@@ -4,3 +4,5 @@ mv out.wav out1.wav
 python music.py
 mv out.wav out2.wav
 sox out1.wav out2.wav out1.wav out2.wav out2.wav out1.wav out.wav fade t 4 0 4
+sox bass.wav newbass.wav gain +8
+sox -m newbass.wav out.wav final.wav
