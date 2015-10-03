3960882930321df2438878f2d2bbccce217c1d9b
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-10-03 16:05:49 -0500

    web-ready

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import subprocess
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_song():
-    return 'heya!'
+    subprocess.call('./song.sh am', shell=True)
+    return """<audio controls><source src="static/song.wav" type="audio/wav"></audio>"""   
 
 if __name__=='__main__':
     app.run()
--- song.sh
+++ song.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 (cd words; ./play.sh albums/$1)
 (cd Music; ./melody.sh)
-sox -m words/out.wav Music/final.wav song.wav
+sox -m words/out.wav Music/final.wav static/song.wav
